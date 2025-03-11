a91a512d0a40d3648d6bfffde2f0897866f57dfd
dearlydan <daniaalika7@gmail.com>
2025-03-11 13:44:45 +0700

    New File

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title', 'Profile Pengguna')
+
+@section('content')
+            <div class="card">
+                <div class="card-header">
+                     <h1>Profil Pengguna</h1>
+               </div>
+               <div class="card-body">
+                    <img src="{{ asset('image/' .$profile['profile_picture']) }}" alt="Profile Picture" width="150">
+                    <h3>{{ $profile['name'] }}</h3>
+                    <p>Email : {{ $profile['email'] }}</p>
+                    <p>Bio : {{ $profile['bio'] }}</p>
+
+                    @if(count($profile['skills']) > 0)
+                    <h5> Skills: </h5>
+                    <ul>
+                        @foreach($profile['skills'] as $skill)
+                        <li>{{ $skill }}</li>
+                        @endforeach
+                   </ul>
+                   @else
+                   <p>Tidak ada skill yang ditampilkan</p>
+                   @endif
+                </div>
+            </div>
+@endsection
